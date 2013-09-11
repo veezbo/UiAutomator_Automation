184cb793b0e973f6c23d52f065e6220851680229
= <vibhor@caltech.edu>
2013-09-11 16:52:42 -0700

    removed necessary name parameter in favor of parsing it out of the path to the project

--- UiAutomator.py
+++ UiAutomator.py
@@ -1,13 +1,13 @@
-import argparse, sys, subprocess
+import argparse, sys, subprocess, os, shutil
 from argparse import RawTextHelpFormatter
 
 
 #Read in Arguments
-parser = argparse.ArgumentParser(description="Description: \n\tBuild and run one or multiple UiAutomator projects at once", epilog="********Example Usages*********\n\npython UiAutomator.py -n Create_Contacts -p /home/vibhor/workspace/Create_Contacts -c contacts.create_contacts\n\npython UiAutomator.py -n Create_Contacts Contacts_Verification -p /home/vibhor/workspace/Create_Contacts /home/vibhor/workspace/Contacts_Verification -c contacts.create_contacts contacts_verification", formatter_class=RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description="Description: \n\tBuild and run one or multiple UiAutomator projects at once", epilog="********Example Usages*********\n\npython UiAutomator.py -n Create_Contacts -p /home/vibhor/workspace/Create_Contacts/ -c contacts.create_contacts\n\npython UiAutomator.py -n Create_Contacts Contacts_Verification -p /home/vibhor/workspace/Create_Contacts/ /home/vibhor/workspace/Contacts_Verification/ -c contacts.create_contacts contacts_verification", formatter_class=RawTextHelpFormatter)
 parser._optionals.title = "Flag Arguments"
-parser.add_argument("-n", help="REQUIRED: project names", metavar="project_names", required=True, nargs="+")
+#parser.add_argument("-n", help="REQUIRED: project names", metavar="project_names", required=True, nargs="+")
 parser.add_argument("-p", help="REQUIRED: full path to projects", metavar="project_paths", required=True, nargs="+")
-parser.add_argument("-c", help="REQUIRED: package names with classes with the tests", metavar="package_names", required=True, nargs="+")
+parser.add_argument("-c", help="REQUIRED: package+class names with the tests", metavar="package_names", required=True, nargs="+")
 
 
 #Print help message if no arguments given
@@ -22,8 +22,8 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 
-#Ensure that we have been given equal numbers of the 3 parameters
-if len(args.n) != len(args.p) or len(args.n) != len(args.c):
+# Ensure that we have been given equal numbers of parameters
+if len(args.p) != len(args.c):
 	print "ERROR: Enter the same number of project names, project paths, and package names"
 	print ""
 	parser.print_help()
@@ -31,11 +31,19 @@ if len(args.n) != len(args.p) or len(args.n) != len(args.c):
 	sys.exit(1)
 
 
+#Get Name from Project
+def getName(p):
+	return p.split('/')[-2]
+
+#Get the names of the projects
+n = map(getName, args.p)
+
+
 #Call the helper script as needed
-num_tests=len(args.n)
+num_tests=len(n)
 ran_tests = 0
 for i in range(0, num_tests):
-	ran_tests += (subprocess.call(["bash", "uiautomator.sh", "-n "+args.n[i], "-p "+args.p[i], "-c "+args.c[i]]) ^ 1)
+	ran_tests += (subprocess.call(["bash", "uiautomator.sh", "-n "+n[i], "-p "+args.p[i], "-c "+args.c[i]]) ^ 1)
 
 
 #Finish
